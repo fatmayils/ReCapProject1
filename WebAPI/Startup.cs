1a834a0c5a12d011048ea7535a06fee029215426
fatmagulyildirimce@gmail.com <fatmagulyildirimce@gmail.com>
2021-03-28 18:51:51 +0300

    Autofac

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,7 +30,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IBrandService, BrandManager>();
+           /* services.AddSingleton<IBrandService, BrandManager>();
             services.AddSingleton<IBrandDal, EfBrandDal>();
 
             services.AddSingleton<ICarService, CarManager>();
@@ -46,7 +46,7 @@ namespace WebAPI
             services.AddSingleton<IRentalDal, EfRentalDal>();
 
             services.AddSingleton<IUserService, UserManager>();
-            services.AddSingleton<IUserDal, EfUserDal>();
+            services.AddSingleton<IUserDal, EfUserDal>();*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
